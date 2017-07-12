ba42724b25378939a4daa2efa9dee192099b1629
Robson Lopes <robson.97.lopes@gmail.com>
2017-07-11 22:31:59 -0300

    First commit

---
+++ chat/assets/render.php
@@ -0,0 +1,11 @@
+<?php
+  header('Content-Type: application/javascript');
+?>
+
+/**************************************************************
+* SIMPLE WEBSITE CHAT
+* https://github.com/robsonlopesmartins/firebase-vuejs-chat
+***************************************************************/
+var RenderChat = (function(){
+
+}());
